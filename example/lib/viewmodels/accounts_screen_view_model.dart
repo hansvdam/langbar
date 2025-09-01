ca6bdd9d9523bbff655212e033345f7e7fad2734
Hans van Dam <hans@damsoft.nl>
2025-09-01 14:51:50 +0200

    add viewmodels

---
+++ example/lib/viewmodels/accounts_screen_view_model.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:langbar_core/ui/cubits/generic_screen_view_model.dart';
+
+class AccountsScreenViewModel extends GenericScreenViewModel<void> {
+  AccountsScreenViewModel({required BuildContext context})
+      : super(null, context: context);
+}
---
+++ example/lib/viewmodels/front_screen_view_model.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:langbar_core/ui/cubits/generic_screen_view_model.dart';
+
+class FrontScreenViewModel extends GenericScreenViewModel<void> {
+  FrontScreenViewModel({required BuildContext context})
+      : super(null, context: context);
+}
