a71fd8e697b103f355597be0dab7e502328959a0
Hans van Dam <hans@damsoft.nl>
2025-09-28 20:27:39 +0200

    add more viewmodels

---
+++ example/lib/viewmodels/contacts_screen_view_model.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:langbar_core/speech_enabled.dart';
+import 'package:langbar_core/utils/utils.dart';
+import 'app_generic_screen_view_model.dart';
+import 'screen_tts_config.dart';
+
+class ContactsScreenState {
+  final String? selectedContact;
+  final String? searchString;
+
+  ContactsScreenState({
+    this.selectedContact,
+    this.searchString,
+  });
+
+  ContactsScreenState copyWith({
+    String? selectedContact,
+    String? searchString,
+  }) {
+    return ContactsScreenState(
+      selectedContact: selectedContact ?? this.selectedContact,
+      searchString: searchString ?? this.searchString,
+    );
+  }
+}
+
+class ContactsScreenViewModel extends AppGenericScreenViewModel<ContactsScreenState>
+    with SpeechEnabled {
+  late final ContactsScreenTtsConfig _ttsConfig;
+
+  ContactsScreenViewModel({
+    required BuildContext context,
+    String? initialSearchString,
+  }) : super(
+          ContactsScreenState(searchString: initialSearchString),
+          context: context,
+        ) {
+    _ttsConfig = ContactsScreenTtsConfig();
+    langbarLogger.i('ContactsScreenViewModel created with search: $initialSearchString');
+
+    // Speak initial search if provided
+    if (initialSearchString != null && initialSearchString.isNotEmpty) {
+      Future.delayed(const Duration(milliseconds: 100), () {
+        speakSearchConfirmation(initialSearchString);
+      });
+    }
+  }
+
+  void updateSearchString(String searchString) {
+    emit(state.copyWith(searchString: searchString));
+  }
+
+  void selectContact(String contactName) {
+    final previousContact = state.selectedContact;
+
+    emit(state.copyWith(selectedContact: contactName));
+
+    // Speak the selection
+    speakConfirmations(contactName, previousContact);
+  }
+
+  void speakConfirmations(String? selectedContact, String? previousContact) {
+    Map<String, dynamic> currentValues = {};
+    Map<String, dynamic> previousValues = {};
+
+    if (selectedContact != null) {
+      currentValues['selectedContact'] = selectedContact;
+      if (previousContact != null) {
+        previousValues['selectedContact'] = previousContact;
+      }
+    }
+
+    _ttsConfig.speakConfirmations(
+      currentValues: currentValues,
+      previousValues: previousValues.isNotEmpty ? previousValues : null,
+      currentScreenCubit: currentScreenCubit,
+    );
+  }
+
+  void speakSearchConfirmation(String searchString) {
+    tts.speak('searching for $searchString');
+  }
+
+  /// Update from constructor parameters (used when navigating with params)
+  void updateFromConstructorParams({String? searchString}) {
+    if (searchString != null && searchString != state.searchString) {
+      final previousSearch = state.searchString;
+      emit(state.copyWith(searchString: searchString));
+
+      if (searchString.isNotEmpty) {
+        speakSearchConfirmation(searchString);
+      }
+    }
+  }
+}
---
+++ example/lib/viewmodels/transactions_screen_view_model.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:langbar_core/speech_enabled.dart';
+import 'package:langbar_core/utils/utils.dart';
+import 'app_generic_screen_view_model.dart';
+import 'screen_tts_config.dart';
+
+class TransactionsScreenState {
+  final String? selectedTransaction;
+  final String? searchFilter;
+  final int? accountId;
+
+  TransactionsScreenState({
+    this.selectedTransaction,
+    this.searchFilter,
+    this.accountId,
+  });
+
+  TransactionsScreenState copyWith({
+    String? selectedTransaction,
+    String? searchFilter,
+    int? accountId,
+  }) {
+    return TransactionsScreenState(
+      selectedTransaction: selectedTransaction ?? this.selectedTransaction,
+      searchFilter: searchFilter ?? this.searchFilter,
+      accountId: accountId ?? this.accountId,
+    );
+  }
+}
+
+class TransactionsScreenViewModel extends AppGenericScreenViewModel<TransactionsScreenState>
+    with SpeechEnabled {
+  late final TransactionsScreenTtsConfig _ttsConfig;
+
+  TransactionsScreenViewModel({
+    required BuildContext context,
+    String? initialFilterString,
+    int? accountId = 1,
+  }) : super(
+          TransactionsScreenState(
+            searchFilter: initialFilterString,
+            accountId: accountId,
+          ),
+          context: context,
+        ) {
+    _ttsConfig = TransactionsScreenTtsConfig();
+    langbarLogger.i('TransactionsScreenViewModel created with filter: $initialFilterString, accountId: $accountId');
+
+    // Speak initial filter if provided
+    if (initialFilterString != null && initialFilterString.isNotEmpty) {
+      Future.delayed(const Duration(milliseconds: 100), () {
+        speakFilterConfirmation(initialFilterString);
+      });
+    }
+  }
+
+  void updateSearchFilter(String filter) {
+    emit(state.copyWith(searchFilter: filter));
+  }
+
+  void selectTransaction(String transactionDescription) {
+    final previousTransaction = state.selectedTransaction;
+
+    emit(state.copyWith(selectedTransaction: transactionDescription));
+
+    // Speak the selection
+    speakConfirmations(
+      selectedTransaction: transactionDescription,
+      previousTransaction: previousTransaction,
+    );
+  }
+
+  void speakConfirmations({
+    String? searchFilter,
+    String? previousFilter,
+    String? selectedTransaction,
+    String? previousTransaction,
+  }) {
+    Map<String, dynamic> currentValues = {};
+    Map<String, dynamic> previousValues = {};
+
+    if (searchFilter != null) {
+      currentValues['searchFilter'] = searchFilter;
+      if (previousFilter != null) {
+        previousValues['searchFilter'] = previousFilter;
+      }
+    }
+
+    if (selectedTransaction != null) {
+      currentValues['selectedTransaction'] = selectedTransaction;
+      if (previousTransaction != null) {
+        previousValues['selectedTransaction'] = previousTransaction;
+      }
+    }
+
+    _ttsConfig.speakConfirmations(
+      currentValues: currentValues,
+      previousValues: previousValues.isNotEmpty ? previousValues : null,
+      currentScreenCubit: currentScreenCubit,
+    );
+  }
+
+  void speakFilterConfirmation(String filter) {
+    speakConfirmations(searchFilter: filter, previousFilter: null);
+  }
+
+  /// Update from constructor parameters (used when navigating with params)
+  void updateFromConstructorParams({String? filterString, int? accountId}) {
+    if (filterString != null && filterString != state.searchFilter) {
+      final previousFilter = state.searchFilter;
+      emit(state.copyWith(searchFilter: filterString));
+
+      if (filterString.isNotEmpty) {
+        speakConfirmations(
+          searchFilter: filterString,
+          previousFilter: previousFilter,
+        );
+      }
+    }
+
+    if (accountId != null && accountId != state.accountId) {
+      emit(state.copyWith(accountId: accountId));
+    }
+  }
+}
